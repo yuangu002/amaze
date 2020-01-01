61020337f5488174b1c8f1d1640450d931879be0
Yuan Gu <ygu03@email.wm.edu>
2020-01-02 01:21:28 +0800

    Initial commit

---
+++ app/src/main/gen/yuanguandziyuezhou/cs301/cs/wm/edu/amazebyyuanguandziyuezhou/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+package yuanguandziyuezhou.cs301.cs.wm.edu.amazebyyuanguandziyuezhou;
+
+/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
+public final class BuildConfig {
+  public final static boolean DEBUG = Boolean.parseBoolean(null);
+}
---
+++ app/src/main/gen/yuanguandziyuezhou/cs301/cs/wm/edu/amazebyyuanguandziyuezhou/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package yuanguandziyuezhou.cs301.cs.wm.edu.amazebyyuanguandziyuezhou;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
